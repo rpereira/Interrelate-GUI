191460b2e8aa626978fbd2a7cba413716fe7d78e
ruiafonsopereira <ruiafonsopereira@gmail.com>
2014-07-02 20:43:48 +0100

    Buttons improvments and some js fixes

--- js/landing.js
+++ js/landing.js
@@ -1,9 +1,9 @@
 /*global jQuery */
 
-// global. currently active menu item
+// current menu item
 var current_item = 0;
 
-// few settings
+// settings
 var section_hide_time = 1300;
 var section_show_time = 1300;
 
@@ -13,18 +13,19 @@ jQuery(document).ready(function($)
     "user strict";
 
 	// Switch section
-	$("a", '.mainmenu').click(function()
+	$("a", ".mainmenu").click(function()
 	{
-		if(!$(this).hasClass('active'))
+		if(!$(this).hasClass("active"))
         {
 			current_item = this;
-			// close all visible divs with the class of .section
-			$('.section:visible').fadeOut(section_hide_time, function()
-            {
-				$('a', '.mainmenu').removeClass('active');
-				$(current_item).addClass('active');
 
-				var new_section = $($(current_item).attr('href'));
+			// close all visible divs with the class of .section
+			$(".section:visible").fadeOut(section_hide_time, function()
+            {
+				$("a", ".mainmenu").removeClass("active");
+				$(current_item).addClass("active");
+
+				var new_section = $($(current_item).attr("href"));
 				new_section.fadeIn(section_show_time);
 			});
 		}
