20e754aac4f0f0591ab42664f7a6f898d251a4fb
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2014-07-05 13:37:47 +0100

    breadcrumb support

--- js/buy-product.js
+++ js/buy-product.js
@@ -1,10 +1,22 @@
-function getProduct(){
-    var _URL = document.URL;
+function getProduct()
+{
+    var _URL  = document.URL;
     var query = _URL.split("?");
     var param = query[1].split("=");
 
     var product = param[1];
+
+    return product;
 }
+
+$(document).ready(function()
+{
+    var product = getProduct();
+
+    var link = "social.html"; //+ product + "-section";
+
+    $("#back-home").click().attr("href", link);
+});
 
 /**
  * Controls step wizard behaviour.
