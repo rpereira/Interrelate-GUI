af7a4bf5ce8a84cce4b0a0af877f4cfa4c47b992
ruiafonsopereira <ruiafonsopereira@gmail.com>
2014-07-09 00:43:48 +0100

    UI improvements

--- js/buy-product.js
+++ js/buy-product.js
@@ -1,3 +1,5 @@
+/*global $ */
+
 var ACTIVE_PACKAGE = '';
 
 $('#btn-login').click(function(e)
@@ -40,47 +42,15 @@ $(document).ready(function()
     setProduct(id);
 });
 
-/**
- * Controls step wizard behaviour.
- */
-$(document).ready(function()
+$("#btn-login").click(function(e)
 {
-    var navListItems = $('ul.setup-panel li a');
-    var allWells     = $('.setup-content');
+    $("#payment-content").addClass('show');
+    $("#navbar-user").addClass('show');
 
-    allWells.hide();
+    $("#payment-content").removeClass('hide');
+    $("#navbar-user").removeClass('hide');
 
-    navListItems.click(function(e)
-    {
-        e.preventDefault();
-
-        var $target = $($(this).attr('href'));
-        var $item   = $(this).closest('li');
-
-        if(!$item.hasClass('disabled'))
-        {
-            navListItems.closest('li').removeClass('active');
-            $item.addClass('active');
-            allWells.hide();
-            $target.show();
-        }
-    });
-
-    $('ul.setup-panel li.active a').trigger('click');
-
-    $('#activate-step-2').on('click', function(e)
-    {
-        $('ul.setup-panel li:eq(1)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#step-2"]').trigger('click');
-        $(this).remove();
-    });
-
-    $('#activate-step-3').on('click', function(e)
-    {
-        $('ul.setup-panel li:eq(2)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#step-3"]').trigger('click');
-        $(this).remove();
-    });
+    $("#btn-login").addClass('hide');
 });
 
 /** Changes colors of the package selected to give feedback */
